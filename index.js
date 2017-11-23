6a02e4f26e188a33ccb3191416607d2365135a56
Foxly1990 <flokistable@protonmail.com>
2017-11-24 01:26:32 +0300

    Examples added...

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const web = express();
+const data = { page: 'Just another game' };
+
+web.use(express.static(path.join(__dirname, 'public')));
+web.get('/', (rq, rs) => rs.render("index.html", data));
+web.listen(8080, () => console.log("Server is up on port 8080"));
