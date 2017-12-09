1befb59702da52e4b616e7aad93e151d7d079ed8
nthiic <info@pstbin.su>
2017-12-09 16:24:33 +0300

    Example scripts path changed

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const web = express();
 
 
 let data = {
-  worldName: "Мир паутин"
+  webWorld: {
+    title: "Мир паутин",
+    scripts: ["Delaunay.js", "WorldWeb.js", "worldWeb.js"]
+  }
 };
 
 web.use("/scripts", express.static( path.join(__dirname, '/node_modules/ramda/dist/') ));
 web.use("/scripts", express.static( path.join(__dirname, '/node_modules/phaser/dist/') ));
 web.use("/scripts", express.static( path.join(__dirname, '/game/') ));
-web.use("/json",    express.static( path.join(__dirname, '/tools/') ));
+web.use("/json",    express.static( path.join(__dirname, '/json/') ));
 web.use("/public",  express.static( path.join(__dirname, '/public/') ));
 
 web.set('view engine', 'jade');
-web.get('/', (req, res) => res.render('world', data));
-web.listen(8000, () => console.log('Server is up on port 8000'));
+web.get('/', (req, res) => res.render('world', data.webWorld));
+web.listen(8080, () => console.log('Server is up on port 8080'));
